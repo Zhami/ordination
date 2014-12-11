f8c10492be078d02058b81f9c7aa1328766a7ad5
Stuart Malin <smalin@akamai.com>
2014-12-11 10:28:10 -0500

    adding functionality for capturing an error

--- examples/completes.js
+++ examples/completes.js
@@ -8,5 +8,39 @@
 
 "use strict";
 
-// to be written
+var ordination		= require('..');
+
+var	Sequencer		= ordination.Sequencer;
+
+function firstIsSync (rqst, accum, next) {
+	console.log('firstIsSync:', 'rqst:', rqst);
+	accum.push("firstIsSync");
+
+	next();
+}
+
+function secondIsAsync (rqst, accum, next) {
+	console.log('secondIsAsync:', 'rqst:', rqst);
+	accum.push("secondIsAsync");
+
+	setTimeout(next, 1000);
+}
+
+function thirdIsFinal (rqst, accum, next) {
+	console.log('thirdIsFinal:', 'accum:', accum);
+
+	// DON'T CALL next()
+}
+
+
+var	steps = [firstIsSync, secondIsAsync, thirdIsFinal];
+
+var seq = new Sequencer(steps);
+
+var rqst = {'i am': 'tbd'};
+
+var accum = [];
+
+
+seq.run(rqst, accum);
 
--- examples/error.js
+++ examples/error.js
@@ -8,5 +8,47 @@
 
 "use strict";
 
-// to be written
+var ordination		= require('..');
+
+var	Sequencer		= ordination.Sequencer;
+
+function firstIsSync (rqst, accum, next) {
+	console.log('firstIsSync:', 'rqst:', rqst);
+	accum.push("firstIsSync");
+
+	next();
+}
+
+function secondHasAsyncError (rqst, accum, next) {
+	console.log('secondIsAsync:', 'rqst:', rqst);
+	accum.push("secondIsAsync");
+
+	function afterTimeout () {
+		next(new Error('Boom!'));
+	}
+
+	setTimeout(afterTimeout, 1000);
+}
+
+function thirdIsFinal (rqst, accum, next) {
+	console.log('thirdIsFinal:', 'accum:', accum);
+
+	// DON'T CALL next()
+}
+
+function ifError (rqst, accum, err) {
+	console.log('had an error:', err);
+	console.log('rqst:', rqst, 'accum:', accum);
+}
+
+var	steps = [firstIsSync, secondHasAsyncError, thirdIsFinal];
+
+var seq = new Sequencer(steps, ifError);
+
+var rqst = {'i am': 'tbd'};
+
+var accum = [];
+
+
+seq.run(rqst, accum);
 
